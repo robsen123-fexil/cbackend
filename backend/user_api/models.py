f0e7c0dd110c79bb71a42d770ff2f77676f731b4
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-08 21:04:07 +0300

    serializer

--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# Our API Models 
+
+class Song(models.Model):
+    artist=models.CharField(max_length=200, default=None)
+    album=models.CharField(max_length=100,  default=None)
+    audio=models.FileField(upload_to='audio/files' , null=True , blank=True, default=None)
+    image = models.ImageField(upload_to='thumbnail/images', null=True, blank=True , default=None)
+    title = models.CharField(max_length=100,blank=False, null=False , default=None)
+    category = models.CharField(max_length=50,blank=False, null=False , default=None)
+    description = models.TextField(blank=True, null=True,  default=None )
+    demo = models.CharField(max_length=150, blank=True, null=True ,  default=None)
+
+    def __str__(self):
+        return self.title
