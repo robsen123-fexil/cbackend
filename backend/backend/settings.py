0b5f81d6d7f2f3c8b949d3ccb3d50a3839b7a8a1
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-09 08:33:36 +0300

    .txst

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,18 +121,21 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
   
 
+
+
+# Default primary key field type
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR , 'staticfiles')
+STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
